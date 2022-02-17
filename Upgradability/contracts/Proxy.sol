c36265f1d5c140fb8c239da8ac42d37c497c3316
Preston Rozwood <279pdbz199@gmail.com>
2022-02-17 00:32:06 -0700

    Shaved off some assembly for readability

--- Upgradability/contracts/Proxy.sol
+++ Upgradability/contracts/Proxy.sol
@@ -5,27 +5,29 @@ contract Proxy {
     // Code position in storage is keccak256("PROXIABLE") = "0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7"
     constructor(bytes memory constructData, address contractLogic) {
         // save the code address
-        assembly { // solium-disable-line
+        assembly {
             sstore(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7, contractLogic)
         }
-        (bool success,) = contractLogic.delegatecall(constructData); // solium-disable-line
+        (bool success,) = contractLogic.delegatecall(constructData);
         require(success, "Construction failed");
     }
 
-    fallback() external payable {
-        assembly { // solium-disable-line
-            let contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
-            calldatacopy(0x0, 0x0, calldatasize())
-            let success := delegatecall(sub(gas(), 10000), contractLogic, 0x0, calldatasize(), 0, 0)
-            let retSz := returndatasize()
-            returndatacopy(0, 0, retSz)
-            switch success
-            case 0 {
-                revert(0, retSz)
-            }
-            default {
-                return(0, retSz)
+    fallback(bytes calldata) external payable returns (bytes memory) {
+        address contractLogic;
+        assembly {
+            contractLogic := sload(0xc5f16f0fcc639fa48a6947836d9850f504798523bf8c9a3a87d5876cf622bcf7)
+        }
+
+        (bool success, bytes memory data) = contractLogic.delegatecall(msg.data);
+        if (!success) {
+            // Uniswap-like decoding of reverted errors in delegatecall
+            assembly {
+                let ptr := mload(0x40)
+                let size := returndatasize()
+                returndatacopy(ptr, 0, size)
+                revert(ptr, size)
             }
         }
+        return data;
     }
 }
